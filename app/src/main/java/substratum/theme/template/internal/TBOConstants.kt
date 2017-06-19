f90fc9ce5cad34301851e3b98bf2d44c8a9a6ed4
spannaa <spannaa@users.noreply.github.com>
2017-06-19 14:46:22 +0100

    Remove EXTRA_PACKAGE_NAMES & Google Photos

--- app/src/main/java/substratum/theme/template/internal/TBOConstants.kt
+++ app/src/main/java/substratum/theme/template/internal/TBOConstants.kt
@@ -9,12 +9,7 @@ object TBOConstants {
             "com.google.android.apps.plus",
             "com.google.android.talk",
             "com.google.android.youtube",
-            "com.google.android.apps.photos",
             "com.google.android.inputmethod.latin"
     )
 
-    val EXTRA_PACKAGE_NAMES = arrayOf(
-            "com.google.android.contacts",
-            "com.google.android.dialer"
-    )
 }
