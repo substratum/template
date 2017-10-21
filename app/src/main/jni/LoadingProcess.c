0739331af8947130f631329949697e62dd962220
Andrzej Ressel <jereksel@gmail.com>
2017-10-21 22:37:36 +0200

    Save

--- app/src/main/jni/LoadingProcess.c
+++ app/src/main/jni/LoadingProcess.c
@@ -1,4 +1,5 @@
 #include <jni.h>
+#include "GeneratedInformation.h"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -9,34 +10,27 @@
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getAPStatus(JNIEnv *env) {
-    return JNI_FALSE;
+    return ENABLE_ANTIPIRACY;
 }
 
 /*
  * Base 64 License Key
  *
- * Insert your base 64 license key obtained from Play Store in the quotations below!
- * ATTENTION: On line 23, do you see the "000"? You MUST count the amount of characters of your key
- * and input the proper number! This is so that you get your own specialized set of variables!
  */
 JNIEXPORT jstring JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getBase64Key(JNIEnv *env) {
-    // TODO: This must be done!
-    char BASE_64_LICENSE_KEY[000] = "";
-    return (*env)->NewStringUTF(env, BASE_64_LICENSE_KEY);
+    return (*env)->NewStringUTF(env, BASE64_LICENSE_KEY);
 }
 
 /*
  * APK Signature Production
  *
  * Insert your release APK's signature in the quotations below!
- * ATTENTION: On line 34, do you see the "28"? If you're disabling antipiracy, change that to 0!
  * All release production signature prefixes have the length of 28!
  */
 JNIEXPORT jstring JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getAPKSignatureProduction(JNIEnv *env) {
-    char APK_SIGNATURE_PRODUCTION[28] = "";
-    return (*env)->NewStringUTF(env, APK_SIGNATURE_PRODUCTION);
+    return (*env)->NewStringUTF(env, APK_SIGNATURE);
 }
 
 /*
@@ -46,7 +40,7 @@ Java_substratum_theme_template_SubstratumLauncher_getAPKSignatureProduction(JNIE
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getInternetCheck(JNIEnv *env) {
-    return JNI_FALSE;
+    return INTERNET_CHECK;
 }
 
 /*
@@ -56,7 +50,7 @@ Java_substratum_theme_template_SubstratumLauncher_getInternetCheck(JNIEnv *env) 
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getGooglePlayRequirement(JNIEnv *env) {
-    return JNI_FALSE;
+    return ENFORCE_GOOGLE_PLAY_INSTALL;
 }
 
 /*
@@ -66,7 +60,7 @@ Java_substratum_theme_template_SubstratumLauncher_getGooglePlayRequirement(JNIEn
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getAmazonAppStoreRequirement(JNIEnv *env) {
-    return JNI_FALSE;
+    return ENFORCE_AMAZON_APP_INSTALL;
 }
 
 /*
@@ -76,7 +70,7 @@ Java_substratum_theme_template_SubstratumLauncher_getAmazonAppStoreRequirement(J
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_getBlacklistedApplications(JNIEnv *env) {
-    return JNI_FALSE;
+    return ENFORCE_BLACKLISTED_APKS_CHECK;
 }
 
 /*
@@ -91,7 +85,7 @@ Java_substratum_theme_template_SubstratumLauncher_getBlacklistedApplications(JNI
  */
 JNIEXPORT jboolean JNICALL
 Java_substratum_theme_template_SubstratumLauncher_allowThirdPartySubstratumBuilds(JNIEnv *env) {
-    return JNI_TRUE;
+    return ALLOW_THIRD_PARTY_SUBSTRATUM_BUILD;
 }
 
 /*
