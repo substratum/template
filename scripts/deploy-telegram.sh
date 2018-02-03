04dd1f7f0a9183e7d91e1f64715201f364d69c53
skittles9823 <skat1@live.com.au>
2018-02-04 04:57:01 +1100

    Who needs incremental versions? 2/2

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-
-cp app/build/outputs/apk/debug/app-debug.apk TelegramX-Mods-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk
-cp app/build/outputs/mapping/debug/mapping.txt proguard-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.txt
+cp app/build/outputs/apk/debug/app-debug.apk
+cp app/build/outputs/mapping/debug/mapping.txt
  
-curl -F chat_id="-1001220326034" -F document=@"TelegramX-Mods-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001083653933" -F document=@"app-debug.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="442800997" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
-cp app/build/outputs/mapping/release/mapping.txt proguard-$MAJOR_MINOR.${TRAVIS_BUILD_NUMBER}_plah.txt
-curl -F chat_id="-1001220326034" -F document=@"proguard-$MAJOR_MINOR.${TRAVIS_BUILD_NUMBER}_plah.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-
+cp app/build/outputs/mapping/release/mapping.txt
+curl -F chat_id="442800997" -F document=@"proguard-release.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+ 
  
