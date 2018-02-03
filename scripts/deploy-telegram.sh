ea86a0c5ee360c3ec2d9e89b2dca9c1e06a34ce6
skittles9823 <skat1@live.com.au>
2018-02-04 06:35:32 +1100

    Not a debug build. Duh!

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-cp app/build/outputs/apk/debug/app-debug.apk
-cp app/build/outputs/mapping/debug/mapping.txt
+cp app/build/outputs/apk/app-debug.apk
+cp app/build/outputs/mapping/mapping.txt
  
 curl -F chat_id="-1001220326034" -F document=@"app-debug.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="427673272" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
