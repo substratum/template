dcc43aea4da4f75c492a3e9623ba71422c487ac8
Till Kottmann <deletescape@outlook.com>
2018-02-03 21:08:48 +0100

    🅱

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
-cp /app/build/outputs/apk/app-debug.apk
-cp /app/build/outputs/mapping/mapping.txt
+cp /app/release/app-release.apk fuckdis.apk
  
-curl -F chat_id="-1001220326034" -F document=@"app-debug.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="427673272" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
- 
-cp /app/build/outputs/mapping/release/mapping.txt
-curl -F chat_id="427673272" -F document=@"proguard-release.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="427673272" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
  
