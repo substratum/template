78dbd6bb4c1efb5156dfe55d4cf0247c2150dd6d
skittles9823 <skat1@live.com.au>
2018-02-04 04:59:54 +1100

    Fix group ID

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -2,10 +2,10 @@
 cp app/build/outputs/apk/debug/app-debug.apk
 cp app/build/outputs/mapping/debug/mapping.txt
  
-curl -F chat_id="-1001083653933" -F document=@"app-debug.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="442800997" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001220326034" -F document=@"app-debug.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="427673272" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
 cp app/build/outputs/mapping/release/mapping.txt
-curl -F chat_id="442800997" -F document=@"proguard-release.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="427673272" -F document=@"proguard-release.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
  
