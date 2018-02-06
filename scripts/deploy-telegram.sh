4449d43c1e54f782f9ff9ca70ec9953899309977
Till Kottmann <deletescape@outlook.com>
2018-02-06 17:41:41 +0100

    Also send it to the dank group

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -2,5 +2,6 @@
 cp app/build/outputs/apk/release/app-release-unsigned.apk fuckdis.apk
  
 curl -F chat_id="427673272" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001220326034" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
  
