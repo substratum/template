8f75bb3c8ca331eddd05c4e9c2b426fc11fa5465
Till Kottmann <till.kottmann@bison-group.com>
2018-02-07 11:38:02 +0100

    I'm dumb af

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cp app/build/outputs/apk/debug/app-debug-unsigned.apk fuckdis.apk
+cp app/build/outputs/apk/debug/app-debug.apk fuckdis.apk
  
 curl -F chat_id="427673272" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="-1001220326034" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
