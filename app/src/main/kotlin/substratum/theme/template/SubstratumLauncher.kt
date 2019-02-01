b3da77b825d6ac6616816de68d26071b12469916
Harsh Shandilya <harsh@prjkt.io>
2019-02-02 03:34:51 +0530

    Add missing appcompat dependency
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/src/main/kotlin/substratum/theme/template/SubstratumLauncher.kt
+++ app/src/main/kotlin/substratum/theme/template/SubstratumLauncher.kt
@@ -7,7 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.util.Log
 import android.widget.Toast
 import com.github.javiersantos.piracychecker.*
